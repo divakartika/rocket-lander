ac9796042a15ab66b016f7ad187db80314d4fc08
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2022-01-17 23:14:23 +0700

    Update rocketlander.py (var0.8)

--- environments/rocketlander.py
+++ environments/rocketlander.py
@@ -380,16 +380,16 @@ class RocketLander(gym.Env):
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
         #           - 0 * abs(state[4]) - 30 * abs(state[5]) \
         #           + 0 * state[6] + 0 * state[7]
-        #var0.7
-        shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
-                  - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
-                  - 0 * abs(state[4]) - 0 * abs(state[5]) \
-                  + 20 * state[6] + 0 * state[7]
-        # #var0.8
+        # #var0.7
         # shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
         #           - 0 * abs(state[4]) - 0 * abs(state[5]) \
-        #           + 0 * state[6] + 20 * state[7]
+        #           + 20 * state[6] + 0 * state[7]
+        #var0.8
+        shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
+                  - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
+                  - 0 * abs(state[4]) - 0 * abs(state[5]) \
+                  + 0 * state[6] + 20 * state[7]
 
         # #var1.1: 0.1 state[0]
         # shaping = -0.1 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
