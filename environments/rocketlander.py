0556823c01ac64dc77b6cfe9a345251498fe66c9
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2022-01-17 20:31:56 +0700

    Update rocketlander.py (var0.7)

--- environments/rocketlander.py
+++ environments/rocketlander.py
@@ -375,16 +375,16 @@ class RocketLander(gym.Env):
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
         #           - 1000 * abs(state[4]) - 0 * abs(state[5]) \
         #           + 0 * state[6] + 0 * state[7]
-        #var0.6
-        shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
-                  - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
-                  - 0 * abs(state[4]) - 30 * abs(state[5]) \
-                  + 0 * state[6] + 0 * state[7]
-        # #var0.7
+        # #var0.6
         # shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
-        #           - 0 * abs(state[4]) - 0 * abs(state[5]) \
-        #           + 20 * state[6] + 0 * state[7]
+        #           - 0 * abs(state[4]) - 30 * abs(state[5]) \
+        #           + 0 * state[6] + 0 * state[7]
+        #var0.7
+        shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
+                  - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
+                  - 0 * abs(state[4]) - 0 * abs(state[5]) \
+                  + 20 * state[6] + 0 * state[7]
         # #var0.8
         # shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
