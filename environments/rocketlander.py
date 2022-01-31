d8572a942cf489abee4c4a6134b20ab1f8bdd224
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2022-01-31 08:52:47 +0700

    Update rocketlander.py

--- environments/rocketlander.py
+++ environments/rocketlander.py
@@ -403,9 +403,9 @@ class RocketLander(gym.Env):
 
         #var1.0: all states ON with state[1] and state[3] negative
         shaping = - 200 * np.sqrt(np.square(state[0]) - np.square(state[1])) \
-                  - 100 * np.sqrt(np.square(state[2]) - np.square(state[3])) \
+                  - 100 * np.sqrt(np.square(state[2]) + np.square(state[3])) \
                   - 1000 * abs(state[4]) - 30 * abs(state[5]) \
-                #   + 20 * state[6] + 20 * state[7]
+                  + 20 * state[6] + 20 * state[7]
 
         # Introduce the concept of options by making reference markers wrt altitude and speed
         # if (state[4] < 0.052 and state[4] > -0.052):
