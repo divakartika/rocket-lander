9151c85ba9670a5d17833444a161126b2fca26c5
Diva Kartika <79572421+divakartika@users.noreply.github.com>
2022-02-09 16:12:13 +0700

    Update rocketlander.py (var1.0)

--- environments/rocketlander.py
+++ environments/rocketlander.py
@@ -385,11 +385,11 @@ class RocketLander(gym.Env):
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
         #           - 1000 * abs(state[4]) - 0 * abs(state[5]) \
         #           + 0 * state[6] + 0 * state[7]
-        #var0.6
-        shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
-                  - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
-                  - 0 * abs(state[4]) - 30 * abs(state[5]) \
-                  + 0 * state[6] + 0 * state[7]
+        # #var0.6
+        # shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
+        #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
+        #           - 0 * abs(state[4]) - 30 * abs(state[5]) \
+        #           + 0 * state[6] + 0 * state[7]
         # #var0.7
         # shaping = -0 * np.sqrt(np.square(state[0]) + 0 * np.square(state[1])) \
         #           - 0 * np.sqrt(np.square(state[2]) + 0* np.square(state[3])) \
@@ -401,9 +401,19 @@ class RocketLander(gym.Env):
         #           - 0 * abs(state[4]) - 0 * abs(state[5]) \
         #           + 0 * state[6] + 20 * state[7]
 
-        # #var1.0: all states ON with state[1] and state[3] negative
-        # shaping = - 200 * np.sqrt(np.square(state[0]) ) \
-        #           - 100 * np.sqrt(np.square(state[2]) ) \
+        #var1.0
+        shaping = - 200 * np.sqrt(state[0] + np.square(state[1])) \
+                  - 100 * np.sqrt(state[2] + np.square(state[3])) \
+                  - 1000 * abs(state[4]) - 30 * abs(state[5]) \
+                  + 20 * state[6] + 20 * state[7]
+        # #var1.1
+        # shaping = - 200 * np.sqrt(state[0]/10 + np.square(state[1])) \
+        #           - 100 * np.sqrt(state[2]/10 + np.square(state[3])) \
+        #           - 1000 * abs(state[4]) - 30 * abs(state[5]) \
+        #           + 20 * state[6] + 20 * state[7]
+        # #var1.2
+        # shaping = - 200 * np.sqrt(state[0]/100 + np.square(state[1])) \
+        #           - 100 * np.sqrt(state[2]/100 + np.square(state[3])) \
         #           - 1000 * abs(state[4]) - 30 * abs(state[5]) \
         #           + 20 * state[6] + 20 * state[7]
 
